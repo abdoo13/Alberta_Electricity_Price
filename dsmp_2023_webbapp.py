ccf602a5aa74616d7e5ee3566afdcc3c1db41adb
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-03-27 17:41:34 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import numpy as np
+import skops.io as sio
 #import sklrn
 #from joblib import load
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
