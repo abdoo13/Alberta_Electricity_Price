d5200b0888f7ac0f7369da9aad9b5577b6218b48
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-03-27 12:51:45 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import numpy as np
-import sklearn
+#import sklrn
 #import joblib
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 data = pd.read_csv('df_.csv')
