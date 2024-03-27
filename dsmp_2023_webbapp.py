1eb04def25a1a4c6c1f24993c71e7e3d2a666663
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-03-27 12:52:23 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import numpy as np
 #import sklrn
-#import joblib
+import joblib
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 data = pd.read_csv('df_.csv')
 # Get column names
