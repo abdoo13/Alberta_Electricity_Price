2193966fdf8e5f1d260005ba17262973e99fd1ec
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-04 11:52:51 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -7,7 +7,7 @@ data = pd.read_csv('df_.csv')
 # Get column names
 column_names = list(data.columns)[1:-1]
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-st.sidebar.markdown('<h2 style="color: blue;"> Select the values of input variables to predict the electricity prices</h2>', unsafe_allow_html=True)
+st.sidebar.markdown('<h2 style="color: blue;"> Select the values of input variables to predict electricity prices</h2>', unsafe_allow_html=True)
 st.sidebar.selectbox(
     "Choose a category?",
     sorted(list(data['Sector'].unique()))
@@ -21,7 +21,7 @@ for column in column_names:
 st.sidebar.button("Predict Electricity Prices") 
 """"""""""""""""""""""""""""""""""""""""""""""""
 st.title('Predicting Electricity Prices in the Alberta Region')
-st.markdown('This app allows predicting Electricty prices for the Alberta region considering the "Residential", "Commercial" and the "Industrial" Sectors.')
+st.markdown('This app allows predicting electricty price for the Alberta region considering "Residential", "Commercial" and "Industrial" Sectors.')
 df = pd.DataFrame()
 list_ = sorted(data['Sector'].unique().tolist())
 #
