cf83c0070d5bcd578d3171d0c73950a188571d2d
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-04 10:50:32 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -15,7 +15,7 @@ for column in column_names:
 
 st.sidebar.button("Predict Electricity Prices")      
 """"""""""""""""""""""""""""""""""""""""""""""""
-st.title('Predicting Electricity Prices at the KKK Alberta Region')
+st.title('Predicting Electricity Prices in the Alberta Region')
 st.markdown('This app allows predicting Electricty prices for the Alberta region considering the "Residential", "Commercial" and the "Industrial" Sectors.')
 
 df = pd.DataFrame()
