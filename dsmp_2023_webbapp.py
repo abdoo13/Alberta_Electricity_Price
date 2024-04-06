b8c83cea567eedcdae9ac613843bd2ad09f12b0b
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-06 12:27:24 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -21,10 +21,10 @@ for column in column_names:
     temp = data[data['Sector'] == sector_]
     user_input_prediction[column] = st.sidebar.slider(f'{column}', round(float(temp[column].min()),0), round(float(temp[column].max()),0), float(temp[column].mean()))
   elif data[column].dtype != 'O' and 'GHG' not in column:
-    user_input_prediction[column] = st.sidebar.slider(f'{column}', float(data[column].min()), float(data[column].max()), float(data[column].mean()))
+    user_input_prediction[column] = st.sidebar.slider(f'{column}', round(float(data[column].min()),0), round(float(data[column].max()),0), float(data[column].mean()))
 
 st.title('Predicting Electricity Prices in the Alberta Region')
-st.markdown('This webb application allows predicting electricty prices in the Alberta region for the following sectors: "Residential", "Commercial" and "Industrial"')
+st.markdown('This webb application allows predicting electricty prices in the Alberta region for the following sectors: "Residential", "Commercial" and "Industrial".')
 
 # Predict Button
 if st.sidebar.button("Predict Electricity Prices"):
