b7a2bdd26c6d80a9cfff10dc0f2e4bc40fb11cd7
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-06 10:35:50 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -8,7 +8,6 @@ data = pd.read_csv('df_.csv')
 # Get column names
 column_names = list(data.columns)[1:-1]
 column_names.remove('Sources')
-st.text(column_names)
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 st.sidebar.markdown('<h2 style="color: blue;"> Select the values of input variables to predict electricity prices</h2>', unsafe_allow_html=True)
 category = st.sidebar.selectbox(
@@ -16,17 +15,15 @@ category = st.sidebar.selectbox(
                               sorted(list(data['Sector'].unique()))
                              )
 sectors = sorted(list(data['Sector'].unique()))
-st.text(sectors)
-ss
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 user_input_prediction = {}
-for column in column_names:
+for column in column_names[1,:]:
   if data[column].dtype != 'O':
     user_input_prediction[column] = st.sidebar.slider(f'{column}', float(data[column].min()), float(data[column].max()), float(data[column].mean()))
 
 st.title('Predicting Electricity Prices in the Alberta Region')
 st.markdown('This app allows predicting electricty price for the Alberta region considering "Residential", "Commercial" and "Industrial" Sectors.')
-
+ss
 # Predict Button
 if st.sidebar.button("Predict Electricity Prices"):
   df = pd.DataFrame()
