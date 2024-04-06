e9c53789c6fcdf3a69949eb7f31ab96a10b56274
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-06 10:37:32 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -23,25 +23,3 @@ for column in column_names[1,:]:
 
 st.title('Predicting Electricity Prices in the Alberta Region')
 st.markdown('This app allows predicting electricty price for the Alberta region considering "Residential", "Commercial" and "Industrial" Sectors.')
-
-# Predict Button
-if st.sidebar.button("Predict Electricity Prices"):
-  df = pd.DataFrame()
-  list_ = sorted(data['Sector'].unique().tolist())
-  #
-  for sector_ in list_:
-    #print(sector_)
-    user_input_prediction['Sector'] = list_.index(sector_)
-    df = pd.concat([df, pd.DataFrame([user_input_prediction])], axis = 0, ignore_index=True)
-  df['Sector'] = labels_.index(category)
-  df['Sector'] = df['Sector'].astype('float')
-  df = df[column_names]
-  st.dataframe(df, hide_index=True)
-
-  #Load the ML Model
-  model = joblib.load('rfr_model.sav')
-
-  #Predict and display the results
-  st.subheader('Prediction')
-  result = model.predict(df.values)
-  st.text(np.round(result[0],2))
