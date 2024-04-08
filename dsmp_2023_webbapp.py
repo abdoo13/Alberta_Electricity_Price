d721dff9c46dad5004c63c174bf6f6898d238baa
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-08 11:16:33 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -64,7 +64,7 @@ if st.sidebar.button("Predict Electricity Prices"):
          strokeOpacity=0)
 
   #st.altair_chart(chart_) #, use_container_width=True)
+  import matplotlib.pyplot as plt
   fig, ax = plt.subplots()
-  from seaborn import barplot
-  ax.barplot(data=df_2, x='Cat', y='Daily End-Use Demand')
+  ax.sns.barplot(data=df_2, x='Cat', y='Daily End-Use Demand')
   st.pyplot(fig)
