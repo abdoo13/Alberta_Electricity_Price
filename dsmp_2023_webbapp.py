fc525f306c3a47c5ac1667e168a750c728546e2a
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-08 11:13:17 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -64,5 +64,6 @@ if st.sidebar.button("Predict Electricity Prices"):
          strokeOpacity=0)
 
   #st.altair_chart(chart_) #, use_container_width=True)
-  sns.barplot(data=df_2, x='Cat', y='Daily End-Use Demand')
-  st.pyplot()
+  fig, ax = plt.subplots()
+  fig = sns.barplot(data=df_2, x='Cat', y='Daily End-Use Demand')
+  st.pyplot(fig)
