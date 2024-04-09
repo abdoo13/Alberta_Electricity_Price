2b3fb012664870b78f1364ffaf4a492029fe07a2
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-09 09:46:49 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -57,5 +57,5 @@ if st.sidebar.button("Predict Electricity Prices"):
   df_3['Daily End-Use Demand (GWh)'] = [df['Daily End-Use Demand (GWh)'][0] + j*(delta_/10) for j in range(0, 11)]
   df_3['Daily GHG Emmisions (Tons_CO2_Equivalent)'] = df_3['Daily End-Use Demand (GWh)']*0.4688
   for col in list(df_3.columns)[2:]:
-    df_3[col] = df[col]
+    df_3[col] = df[col][0]
   st.dataframe(df_3)
