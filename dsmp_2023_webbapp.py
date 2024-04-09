357708b216ceeb648c406d0f0241e511cce139b5
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-09 10:21:03 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -39,7 +39,7 @@ if st.sidebar.button("Predict Electricity Prices"):
   df['Daily GHG Emmisions (Tons_CO2_Equivalent)'] = df['Daily End-Use Demand (GWh)']*0.4688
   #
   df = df[column_names[1:]]
-  st.dataframe(df, hide_index=True)
+  #st.dataframe(df, hide_index=True)
 
   #Load the ML Model
   model = joblib.load('rfr_model.sav')
@@ -50,7 +50,7 @@ if st.sidebar.button("Predict Electricity Prices"):
   #st.text(np.round(result,2))
   df_2 = pd.DataFrame({'Cat':['Low', 'Price', 'Top'], 'Daily End-Use Demand':df['Daily End-Use Demand (GWh)'], 'Electricity Price':np.round(result,2)})
   st.dataframe(df_2)
-  st.bar_chart(df_2, x='Cat', y=['Daily End-Use Demand', 'Electricity Price'])
+  #st.bar_chart(df_2, x='Cat', y=['Daily End-Use Demand', 'Electricity Price'])
   
   delta_ = df_2['Daily End-Use Demand'][2] - df_2['Daily End-Use Demand'][0]
   df_3 = pd.DataFrame(columns=column_names[1:])
