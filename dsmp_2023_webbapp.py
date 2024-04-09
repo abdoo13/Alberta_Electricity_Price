a49d0e0306367ad5331bbff019d133cc8136075c
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-09 10:17:26 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -66,3 +66,4 @@ if st.sidebar.button("Predict Electricity Prices"):
                                         x = alt.X(cols_3[0]),
                                         y = alt.Y(cols_3[-1], scale=alt.Scale(domain=[10, 20]))
                                         ))
+  st.altair_chart(c)
