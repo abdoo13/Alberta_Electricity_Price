0d0d8a6f1f7dcb9b6b331aac08694e515480d670
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-11 10:17:30 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -60,7 +60,7 @@ if st.sidebar.button("Predict Electricity Prices"):
     value = df_2['Daily End-Use Demand'][1],
     title = {'text': "Daily End-Use Demand (GWh)"},
     domain = {'x': [0, 1], 'y': [0, 1]},
-    gauge = {'axis': {'range': [np.round(df_2['Daily End-Use Demand'][1],0)-2, np.round(df_2['Daily End-Use Demand'][1],0)+2]}, 'bar': {'color': "royalblue"}}
+    gauge = {'axis': {'range': [np.round(df_2['Daily End-Use Demand'][1],0)-2, np.round(df_2['Daily End-Use Demand'][1],0)+2]}, 'bar': {'color': "red"}}
   ))
   fig2 = go.Figure(go.Indicator(
     mode = "gauge+number",
