bf9ce15177445aa899f3a28f700a8f4368a13bf0
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-04-14 16:02:36 +0100

    Update Fun_Sector_EnergyUse.py

--- Fun_Sector_EnergyUse.py
+++ Fun_Sector_EnergyUse.py
@@ -1,4 +1,4 @@
 def Sector_EnergyUse(sector_):
-  temp = data[data['Sector'] == sector_]
+  #temp = data[data['Sector'] == sector_]
   #return(np.round(temp['Daily End-Use Demand (GWh)'].min(),3), np.round(temp['Daily End-Use Demand (GWh)'].max(),3), np.round(temp['Daily End-Use Demand (GWh)'].mean(),3))
   return(sector_)
